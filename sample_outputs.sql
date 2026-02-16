4292f005bf2cf202bf5cab715146603c205bcc94
gatatibhargavi3 <gatatibhargavi1703@gmail.com>
2026-02-16 20:08:10 +0530

    Add files via upload

---
+++ sample_outputs.sql
@@ -0,0 +1,344 @@
+-- BEGINNER LEVEL QUERIES (Basic SELECT, WHERE, ORDER BY)
+
+-- 1.Show all theatres in Mumbai
+SELECT * FROM THEATRES WHERE CITY = 'MUMBAI';
+Output:
++------------+--------------+--------+
+| theatre_id | theatre_name | city   |
++------------+--------------+--------+
+|          2 | INOX         | Mumbai |
++------------+--------------+--------+
+
+-- 2.List shows with ticket price > 300
+SELECT show_id, ticket_price
+FROM Shows
+WHERE ticket_price > 300;
+Output:
++---------+--------------+
+| show_id | ticket_price |
++---------+--------------+
+|       3 |       350.00 |
+|       5 |       320.00 |
++---------+--------------+
+
+-- 3.Display customers sorted by name
+SELECT * FROM Customers
+ORDER BY name;
+Output:
++-------------+--------------+-----------------+------------+
+| customer_id | name         | email           | phone      |
++-------------+--------------+-----------------+------------+
+|           3 | Arjun Verma  | arjun@gmail.com | 9876543333 |
+|           5 | Karan Patel  | karan@gmail.com | 9876545555 |
+|           2 | Priya Mehta  | priya@gmail.com | 9876543222 |
+|           1 | Rahul Sharma | rahul@gmail.com | 9876543210 |
+|           4 | Sneha Reddy  | sneha@gmail.com | 9876544444 |
++-------------+--------------+-----------------+------------+
+
+-- 4.Show movies in Hindi
+SELECT title FROM Movies
+WHERE language = 'Hindi';
+Output:
++-------+
+| title |
++-------+
+| Jawan |
++-------+
+
+-- 5.Show total seats in each screen
+SELECT screen_name, total_seats
+FROM Screens;
+Output:
++-------------+-------------+
+| screen_name | total_seats |
++-------------+-------------+
+| Screen 1    |         150 |
+| Screen 1    |         180 |
+| Screen 2    |         200 |
+| Screen 1    |         120 |
+| Screen 3    |         160 |
++-------------+-------------+
+
+-- 6.Display bookings made on '2024-02-19'
+SELECT * FROM Bookings
+WHERE booking_date = '2024-02-19';
+Output:
++------------+---------+-------------+--------------+-------------------+--------------+
+| booking_id | show_id | customer_id | booking_date | number_of_tickets | total_amount |
++------------+---------+-------------+--------------+-------------------+--------------+
+|          1 |       1 |           1 | 2024-02-19   |                 2 |       500.00 |
+|          2 |       2 |           2 | 2024-02-19   |                 3 |       900.00 |
++------------+---------+-------------+--------------+-------------------+--------------+
+
+-- 7.Show distinct languages
+SELECT DISTINCT language FROM Movies;
+Output:
++----------+
+| language |
++----------+
+| Tamil    |
+| Hindi    |
+| English  |
+| Kannada  |
+| Telugu   |
++----------+
+
+-- 8.Count total customers
+SELECT COUNT(*) AS total_customers
+FROM Customers;
+Output:
++-----------------+
+| total_customers |
++-----------------+
+|               5 |
++-----------------+
+
+-- INTERMEDIATE LEVEL QUERIES (JOINS, GROUP BY, Aggregations)
+
+-- 1.Show movie name with show date & price
+SELECT m.title, s.show_date, s.ticket_price
+FROM Shows s
+JOIN Movies m ON s.movie_id = m.movie_id;
+Output:
++-------------------+------------+--------------+
+| title             | show_date  | ticket_price |
++-------------------+------------+--------------+
+| Leo               | 2024-02-20 |       250.00 |
+| Jawan             | 2024-02-20 |       300.00 |
+| Avengers: Endgame | 2024-02-21 |       350.00 |
+| KGF Chapter 2     | 2024-02-21 |       280.00 |
+| Pushpa 2          | 2024-02-22 |       320.00 |
++-------------------+------------+--------------+
+
+-- 2.Show theatre name with screen count
+SELECT t.theatre_name, COUNT(s.screen_id) AS total_screens
+FROM Theatres t
+JOIN Screens s ON t.theatre_id = s.theatre_id
+GROUP BY t.theatre_name;
+Output:
++---------------+---------------+
+| theatre_name  | total_screens |
++---------------+---------------+
+| PVR Cinemas   |             1 |
+| INOX          |             1 |
+| Cinepolis     |             1 |
+| Miraj Cinemas |             1 |
+| Asian Cinemas |             1 |
++---------------+---------------+
+
+-- 3.Total tickets booked per show
+SELECT show_id, SUM(number_of_tickets) AS total_tickets
+FROM Bookings
+GROUP BY show_id;
+Output:
++---------+---------------+
+| show_id | total_tickets |
++---------+---------------+
+|       1 |             2 |
+|       2 |             3 |
+|       3 |             1 |
+|       4 |             4 |
+|       5 |             2 |
++---------+---------------+
+
+-- 4.Total revenue generated
+SELECT SUM(total_amount) AS total_revenue
+FROM Bookings;
+Output:
++---------------+
+| total_revenue |
++---------------+
+|       3510.00 |
++---------------+
+
+-- 5.Movie with highest ticket price
+SELECT m.title, MAX(s.ticket_price) AS highest_price
+FROM Shows s
+JOIN Movies m ON s.movie_id = m.movie_id
+GROUP BY m.title;
+Output:
++-------------------+---------------+
+| title             | highest_price |
++-------------------+---------------+
+| Leo               |        250.00 |
+| Jawan             |        300.00 |
+| Avengers: Endgame |        350.00 |
+| KGF Chapter 2     |        280.00 |
+| Pushpa 2          |        320.00 |
++-------------------+---------------+
+
+-- 6.Customer name with number of bookings
+SELECT c.name, COUNT(b.booking_id) AS bookings
+FROM Customers c
+JOIN Bookings b ON c.customer_id = b.customer_id
+GROUP BY c.name;
+Output:
++--------------+----------+
+| name         | bookings |
++--------------+----------+
+| Rahul Sharma |        1 |
+| Priya Mehta  |        1 |
+| Arjun Verma  |        1 |
+| Sneha Reddy  |        1 |
+| Karan Patel  |        1 |
++--------------+----------+
+
+-- 7.Total tickets booked by each customer
+SELECT c.name, SUM(b.number_of_tickets) AS total_tickets
+FROM Customers c
+JOIN Bookings b ON c.customer_id = b.customer_id
+GROUP BY c.name;
+Output:
++--------------+---------------+
+| name         | total_tickets |
++--------------+---------------+
+| Rahul Sharma |             2 |
+| Priya Mehta  |             3 |
+| Arjun Verma  |             1 |
+| Sneha Reddy  |             4 |
+| Karan Patel  |             2 |
++--------------+---------------+
+
+-- 8.Show movie with theatre name
+SELECT m.title, t.theatre_name
+FROM Movies m
+JOIN Shows s ON m.movie_id = s.movie_id
+JOIN Screens sc ON s.screen_id = sc.screen_id
+JOIN Theatres t ON sc.theatre_id = t.theatre_id;
+Output:
++-------------------+---------------+
+| title             | theatre_name  |
++-------------------+---------------+
+| Leo               | PVR Cinemas   |
+| Jawan             | INOX          |
+| Avengers: Endgame | Cinepolis     |
+| KGF Chapter 2     | Miraj Cinemas |
+| Pushpa 2          | Asian Cinemas |
++-------------------+---------------+
+
+-- ADVANCED LEVEL QUERIES (Subqueries, Analytics, Revenue Logic)
+
+-- 1.Movie generating highest revenue
+SELECT m.title, SUM(b.total_amount) AS revenue
+FROM Movies m
+JOIN Shows s ON m.movie_id = s.movie_id
+JOIN Bookings b ON s.show_id = b.show_id
+GROUP BY m.title
+ORDER BY revenue DESC
+LIMIT 1;
+Output:
++---------------+---------+
+| title         | revenue |
++---------------+---------+
+| KGF Chapter 2 | 1120.00 |
++---------------+---------+
+
+-- 2.Top paying customer
+SELECT c.name, SUM(b.total_amount) AS total_spent
+FROM Customers c
+JOIN Bookings b ON c.customer_id = b.customer_id
+GROUP BY c.name
+ORDER BY total_spent DESC
+LIMIT 1;
+Output:
++-------------+-------------+
+| name        | total_spent |
++-------------+-------------+
+| Sneha Reddy |     1120.00 |
++-------------+-------------+
+
+-- 3.Show occupancy percentage per show
+SELECT 
+    s.show_id,
+    (SUM(b.number_of_tickets) / sc.total_seats) * 100 AS occupancy_percentage
+FROM Shows s
+JOIN Screens sc ON s.screen_id = sc.screen_id
+JOIN Bookings b ON s.show_id = b.show_id
+GROUP BY s.show_id, sc.total_seats;
+Output:
++---------+----------------------+
+| show_id | occupancy_percentage |
++---------+----------------------+
+|       1 |               1.3333 |
+|       2 |               1.6667 |
+|       3 |               0.5000 |
+|       4 |               3.3333 |
+|       5 |               1.2500 |
++---------+----------------------+
+
+-- 4.Movies with revenue greater than 500
+SELECT m.title, SUM(b.total_amount) AS revenue
+FROM Movies m
+JOIN Shows s ON m.movie_id = s.movie_id
+JOIN Bookings b ON s.show_id = b.show_id
+GROUP BY m.title
+HAVING revenue > 500;
+Output:
++---------------+---------+
+| title         | revenue |
++---------------+---------+
+| Jawan         |  900.00 |
+| KGF Chapter 2 | 1120.00 |
+| Pushpa 2      |  640.00 |
++---------------+---------+
+
+-- 5.Find movies not booked yet
+SELECT title
+FROM Movies
+WHERE movie_id NOT IN (
+    SELECT DISTINCT movie_id FROM Shows s
+    JOIN Bookings b ON s.show_id = b.show_id
+);
+Output: Empty set (0.01 sec)
+
+-- 6.Total revenue per city
+SELECT t.city, SUM(b.total_amount) AS city_revenue
+FROM Theatres t
+JOIN Screens sc ON t.theatre_id = sc.theatre_id
+JOIN Shows s ON sc.screen_id = s.screen_id
+JOIN Bookings b ON s.show_id = b.show_id
+GROUP BY t.city;
+Output:
++-----------+--------------+
+| city      | city_revenue |
++-----------+--------------+
+| Delhi     |       500.00 |
+| Mumbai    |       900.00 |
+| Bangalore |       350.00 |
+| Hyderabad |      1120.00 |
+| Chennai   |       640.00 |
++-----------+--------------+
+
+-- 7.Most popular movie (most tickets sold)
+SELECT m.title, SUM(b.number_of_tickets) AS tickets_sold
+FROM Movies m
+JOIN Shows s ON m.movie_id = s.movie_id
+JOIN Bookings b ON s.show_id = b.show_id
+GROUP BY m.title
+ORDER BY tickets_sold DESC
+LIMIT 1;
+Output:
++---------------+--------------+
+| title         | tickets_sold |
++---------------+--------------+
+| KGF Chapter 2 |            4 |
++---------------+--------------+
+
+-- 8.Rank movies by revenue
+SELECT m.title,
+       SUM(b.total_amount) AS revenue
+FROM Movies m
+JOIN Shows s ON m.movie_id = s.movie_id
+JOIN Bookings b ON s.show_id = b.show_id
+GROUP BY m.title
+ORDER BY revenue DESC;
+Output:
++-------------------+---------+
+| title             | revenue |
++-------------------+---------+
+| KGF Chapter 2     | 1120.00 |
+| Jawan             |  900.00 |
+| Pushpa 2          |  640.00 |
+| Leo               |  500.00 |
+| Avengers: Endgame |  350.00 |
++-------------------+---------+
